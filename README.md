5dbf47c566735e0f1625c9f55c9a45f0785f488d
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-10-15 07:10:18 +0200

    Ändra från smtp till API mail

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # flask-webshop
 En enkel webbshop byggd med Flask, skapad med Python och Flask-ramverket.
 Stylat med Bootstrap för en ren och responsiv design.
+
+Installera:
+pip install resend
+
--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 
 from dotenv import load_dotenv
 from flask import Flask, render_template
-from flask_mail import Mail, Message
 import os
 from flask import request, redirect, url_for
+import resend
 
 app = Flask(__name__)
-mail = Mail(app)
-
 load_dotenv()
 
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-mail = Mail(app)
+resend.api_key = os.getenv("RESEND_API_KEY")
+
+r = resend.Emails.send({
+  "from": "onboarding@resend.dev",
+  "to": "filip_nordqvist@hotmail.com",
+  "subject": "Hello World",
+  "html": "<p>Congrats on sending your <strong>first email</strong>!</p>"
+})
 
 
 @app.route('/')
@@ -39,17 +38,22 @@ def contact():
 def send_email():
     email = request.form.get('email')
     message = request.form.get('message')
+
     if not email or not message:
         return "Email and message required", 400
 
-    msg = Message(
-        'New message from webshop',
-        sender=app.config['MAIL_USERNAME'],
-        recipients=[email]
-    )
-    msg.body = message
-    mail.send(msg)
-    return redirect(url_for('contact'))
+    try:
+        r = resend.Emails.send({
+            "from": "hnf.sweden@gmail.com",
+            "to": email,
+            "subject": "New message from webshop",
+            "html": f"<p>{message}</p>"
+        })
+        print(r)  # För loggning/debug
+        return redirect(url_for('contact'))
+    except Exception as e:
+        print("Error:", e)
+        return "Failed to send email", 500
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
